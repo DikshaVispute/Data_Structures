5a9265f3e71d766766623798efa98bd7343ae20a
DikshaVispute <dikshavispute809@gmail.com>
2025-12-26 16:18:11 +0530

    Add ClassWork Day4 & 5

---
+++ Class_Work/Day_04/program404.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE first, PPNODE last, int no)
+{
+    
+}
+
+void InsertLast(PPNODE first, PPNODE last, int no)
+{
+    
+}
+
+void DeleteFirst(PPNODE first,PPNODE last)
+{
+}
+
+void DeleteLast(PPNODE first,PPNODE last)
+{
+    
+    
+}
+
+void Display(PNODE first,PNODE last )
+{
+    
+    
+}
+
+int Count(PNODE first,PNODE last)
+{
+    
+
+    return 0;
+}
+
+void InsertAtPos(PPNODE first, int no, int pos)
+{
+   
+      
+}
+
+void DeleteAtPos(PPNODE first, PPNODE last, int pos)
+{
+    
+    
+}
+
+int main()
+{
+
+
+    return 0;
+}
